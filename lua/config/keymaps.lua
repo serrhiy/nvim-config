db7a830e50d75295110a73d46ce35b437a383a41
Serhii Lytvynenko <serhii.lytvynenko-ext@jci.com>
2026-09-11 15:54:18 +0000

    Add single-chord window navigation keymaps
    <C-hjkl> instead of the native two-chord <C-w> then h/j/k/l, mainly
    for switching between the nvim-tree window and code quickly.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,7 @@
 vim.keymap.set("n", "gb", "<C-o>", { desc = "Go back" })
+
+-- Single-chord window navigation (native Neovim default is <C-w> then h/j/k/l)
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
