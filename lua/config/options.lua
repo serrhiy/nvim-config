d089679a4a0994ab29d703e7aebc256b060aa6b3
Serhii Lytvynenko <serhii.lytvynenko-ext@jci.com>
2026-09-11 16:56:12 +0300

    Setup basic neovim options

---
+++ lua/config/options.lua
@@ -0,0 +1,29 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.cursorline = true
+vim.o.termguicolors = true
+vim.o.scrolloff = 8
+vim.o.wrap = false
+
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.smartindent = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undofile = true
+vim.o.undodir = vim.fn.stdpath("state") .. "/undo"
+
+vim.o.updatetime = 250
+
+vim.o.clipboard = "unnamedplus"
